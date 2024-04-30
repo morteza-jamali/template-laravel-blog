70939e84adf7f1924069ac53379b19c25791de0c
Morteza Jamali <mortezajamali4241@gmail.com>
2024-04-30 07:48:18 +0000

    feat(dashboard): create components

---
+++ resources/js/Components/Dashboard/Logo/Logo.tsx
@@ -0,0 +1,34 @@
+import classes from './Logo.module.css';
+import {
+  Group,
+  Text,
+  UnstyledButton,
+  UnstyledButtonProps,
+} from '@mantine/core';
+
+type LogoProps = {
+  href?: string;
+} & UnstyledButtonProps;
+
+const Logo = ({ href, ...others }: LogoProps) => {
+  return (
+    <UnstyledButton
+      className={classes.logo}
+      component={Link}
+      href={href || '/'}
+      {...others}
+    >
+      <Group gap="xs">
+        <img
+          src="https://mantine-analytics-dashboard.netlify.app/_next/image?url=%2Flogo-no-background.png&w=48&q=75"
+          alt=""
+          height={24}
+          width={24}
+        />
+        <Text fw={700}>Mantine admin</Text>
+      </Group>
+    </UnstyledButton>
+  );
+};
+
+export default Logo;
---
+++ resources/js/Components/Dashboard/index.ts
@@ -0,0 +1,73 @@
+import Logo from './Logo/Logo';
+import ToggleTheme from './ToggleTheme/ToggleTheme';
+import LanguagePicker from './LanguagePicker/LanguagePicker';
+import FilterDateMenu from './FilterDateMenu/FilterDateMenu';
+import StatsGrid from './StatsGrid/StatsGrid';
+import RevenueChart from './RevenueChart/RevenueChart';
+import SalesChart from './SalesChart/SalesChart';
+import MobileDesktopChart from './MobileDesktopChart/MobileDesktopChart';
+import ProjectsTable from './ProjectsTable/ProjectsTable';
+import StatsCard from './StatsCard/StatsCard';
+import MapChart from './MapChart/MapChart';
+import LanguageTable from './LanguageTable/LanguageTable';
+import TrafficTable from './TrafficTable/TrafficTable';
+import UserProfileCard from './UserProfileCard/UserProfileCard';
+import PricingCard from './PricingCard/PricingCard';
+import ChatsList from './ChatsList/ChatsList';
+import ChatItem from './ChatItem/ChatItem';
+import ProjectsCard from './ProjectsCard/ProjectsCard';
+import AddTaskCard from './AddTaskCard/AddTaskCard';
+import KanbanColumn from './KanbanColumn/KanbanColumn';
+import KanbanCard from './KanbanCard/KanbanCard';
+import KanbanBoard from './KanbanBoard/KanbanBoard';
+import OrdersTable from './OrdersTable/OrdersTable';
+import InvoicesTable from './InvoicesTable/InvoicesTable';
+import InvoiceDetailsCard from './InvoiceDetailsCard/InvoiceDetailsCard';
+import UserProfileButton from './UserButton/UserButton';
+import ThemeDrawer from './ThemeDrawer/ThemeDrawer';
+import PageHeader from './PageHeader/PageHeader';
+import RouterTransition from './RouterTransition/RouterTransition';
+import ProfileStatsCard from './ProfileStatsCard/ProfileStatsCard';
+import Faqs from './Faqs/Faqs';
+import Surface from './Surface';
+import TextEditor from './TextEditor';
+import UserButton from './UserButton';
+import ErrorAlert from './ErrorAlert';
+
+export {
+  Logo,
+  ToggleTheme,
+  LanguagePicker,
+  FilterDateMenu,
+  StatsGrid,
+  RevenueChart,
+  SalesChart,
+  MobileDesktopChart,
+  ProjectsTable,
+  StatsCard,
+  MapChart,
+  LanguageTable,
+  TrafficTable,
+  UserProfileCard,
+  PricingCard,
+  ChatsList,
+  ChatItem,
+  ProjectsCard,
+  AddTaskCard,
+  KanbanColumn,
+  KanbanCard,
+  KanbanBoard,
+  OrdersTable,
+  InvoicesTable,
+  InvoiceDetailsCard,
+  UserProfileButton,
+  ThemeDrawer,
+  PageHeader,
+  RouterTransition,
+  ProfileStatsCard,
+  Faqs,
+  Surface,
+  TextEditor,
+  UserButton,
+  ErrorAlert,
+};
